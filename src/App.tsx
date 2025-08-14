388b8286e7d69fee91af670942643a51f5a0d2f6
Simon Phillips <simonxphillips@gmail.com>
2025-08-13 22:12:05 -0400

    Added text

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,12 @@ import PokemonPicker from "./PokemonPicker";
 export default function App() {
   return (
     <main style={{ padding: 24, fontFamily: "system-ui, sans-serif" }}>
-      <h1>Simple Pokédex (beta)</h1>
+      <h1>Simon's Simple Pokédex (beta)</h1>
+      <p>This app was built by Simon Phillips.</p><a href="https://github.com/tassyguy">Check out my GitHub repo for more coding projects!</a>
+      <p>This was built using TypeScript, React, Vite, GitHub Actions, the PokéAPI, Node.js, and pure determination.</p>
       <PokemonPicker />
       <h3>Please note this app is actively being worked on and will have errors (such as missing and/or excess Pokémon entries)</h3>
+      <h3>More features will be added as time goes along</h3>
     </main>
   );
 }
